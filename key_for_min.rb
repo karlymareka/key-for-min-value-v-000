09267cb902da6fcc987f06b1382be28f6be637d4
Karly Mareka <karly.mareka@gmail.com>
2018-09-08 18:15:41 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,14 +5,15 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     return nil 
   else 
-  minimum = 99999999
-  minimum_key = ""
-  name_hash.collect do |key, value|
-    if value < minimum
-      minimum = value
-      minimum_key = key 
-    end 
-  end
-  return minimum_key 
+    minimum = 99999999
+    minimum_key = ""
+    name_hash.collect do |key, value|
+      if value < minimum
+        minimum = value
+        minimum_key = key 
+      end 
+    end
+    return minimum_key
+  end 
 end
 
